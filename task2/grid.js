bf48775c5b5e4eebf70d63f9823168de64d753b8
dennissimmo <denchik16062001@gmail.com>
2022-04-10 16:26:48 +0300

    [#0] Replace aray of letters on string, because string behaves like an array of symbols

--- task2/grid.js
+++ task2/grid.js
@@ -5,13 +5,7 @@ function grid(dimension) {
     if (dimension < 0) {
         return null;
     }
-    const letters = [
-        'a', 'b', 'c', 'd', 'e', 'f',
-        'g', 'h', 'i', 'j', 'k', 'l',
-        'm', 'n', 'o', 'p', 'q', 'r',
-        's', 't', 'u', 'v', 'w', 'x',
-        'y', 'z'
-    ];
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
     const stepDivider = letters.length;
     const matrix = [];
     for (let i = 0; i < dimension; i++) {
@@ -26,4 +20,4 @@ function grid(dimension) {
     return matrix.join('\n');
 }
 
-console.log(grid(10));
+console.log(grid(5));
